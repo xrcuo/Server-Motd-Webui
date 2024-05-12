a5ff743bfe3805cbb5825a9bc4a05e5f8542b702
xrcuo <836447856@qq.com>
2024-05-12 10:09:51 +0800

    url

--- main.go
+++ main.go
@@ -2,7 +2,46 @@ package webui
 
 import (
 	"embed"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 //go:embed static
 var Dist embed.FS
+
+// 下载
+func Download(url string, ws string) {
+
+	// 发送请求
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// 创建一个文件用于保存
+	out, err := os.Create(ws)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	// 然后将响应流和文件流对接起来
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+// 创建目录
+func CreateDirectory(directoryPath string) {
+	err := os.Mkdir(directoryPath, 0755)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
